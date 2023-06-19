821d0d00c9cf208ac3bce9e4f21d9fa482be265f
Yoon Robin <robinyoonofficial@gmail.com>
2023-06-19 11:19:25 +0900

    Feat: Get data by using fetch method

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,7 +1,57 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Suspense } from "react";
+import config from "@/config/serviceKey.config";
 
-export default function Home() {
+interface MsgInfo {
+  create_date: string;
+  location_id: string; //숫자로
+  location_name: string;
+  md101_sn: string; //숫자로
+  msg: string;
+  send_platform: string;
+}
+
+async function getData(serviceKey: string) {
+  const url =
+    "https://apis.data.go.kr/1741000/DisasterMsg3/getDisasterMsg1List";
+  let pageNo = 1;
+  let numOfRows = 10;
+
+  const finalURL =
+    url +
+    `?serviceKey=${serviceKey}&pageNo=${pageNo}&numOfRows=${numOfRows}&type=json`;
+
+  console.log("finalURL: " + finalURL);
+
+  const res = await fetch(finalURL, {
+    next: { revalidate: 10 },
+  });
+
+  const data = res.json();
+
+  return data;
+}
+
+export default async function Home() {
+  const serviceKey = config.serviceKey;
+
+  const data = await getData(serviceKey);
+
+  // head
+  const head = data.DisasterMsg[0].head;
+  console.log("---head---");
+  console.log(head);
+
+  const totalCount = head[0].totalCount;
+  console.log("totalCount: " + totalCount);
+
+  // row
+  const row = data.DisasterMsg[1].row;
+
+  const item1: MsgInfo = row[0];
+  console.log(item1);
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -11,36 +61,41 @@ export default function Home() {
         </p>
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://github.com/robinyoon-dev/disaster-msg-project"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
+              src="/github.svg"
+              alt="GitHub Logo"
+              className={styles.githubLogo}
+              width={24}
               height={24}
               priority
-            />
+            />{" "}
+            GitHub
           </a>
         </div>
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <h1>오늘의 재난 문자 발송</h1>
+        <p></p>
+
+        <Suspense fallback={<p>불러오는 중입니다...</p>}>
+          {data && (
+            <>
+              <p>데이터가 들어왔어요</p>
+
+              <p>전체 결과</p>
+              <p>{totalCount}</p>
+            </>
+          )}
+        </Suspense>
       </div>
 
       <div className={styles.grid}>
-        <a
+        {/* <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
           target="_blank"
@@ -88,7 +143,7 @@ export default function Home() {
           <p>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
-        </a>
+        </a> */}
       </div>
     </main>
   );
---
+++ my-app/config/serviceKey.config.ts
@@ -0,0 +1,6 @@
+const config = {
+  serviceKey: process.env.SERVICE_KEY,
+  url: process.env.URL,
+};
+
+export default config;
