2d2a76415f6da840abf749023e59eee68a9a8e6b
Yoon Robin <robinyoonofficial@gmail.com>
2023-09-12 13:16:44 +0900

    Feat: Add getKeywords function

---
+++ my-app/app/getKeywords.ts
@@ -0,0 +1,92 @@
+export default function getKeywords(text: string) {
+  if (
+    text.includes("☎182") &&
+    (text.includes("실종된") ||
+      text.includes("배회중인") ||
+      text.includes("목격된"))
+  ) {
+    return "실종";
+  } else if (text.includes("호우")) {
+    return "호우";
+  } else if (text.includes("대설")) {
+    return "대설";
+  } else if (text.includes("강풍")) {
+    return "강풍";
+  } else if (text.includes("태풍")) {
+    return "태풍";
+  } else if (text.includes("해일")) {
+    return "해일";
+  } else if (text.includes("폭풍우")) {
+    return "폭풍우";
+  } else if (text.includes("지진")) {
+    return "지진";
+  } else if (text.includes("돌풍")) {
+    return "돌풍";
+  } else if (text.includes("지진해일")) {
+    return "지진해일";
+  } else if (text.includes("황사")) {
+    return "황사";
+  } else if (text.includes("산사태")) {
+    return "산사태";
+  } else if (text.includes("홍수")) {
+    return "홍수";
+  } else if (text.includes("가뭄")) {
+    return "가뭄";
+  } else if (text.includes("우박")) {
+    return "우박";
+  } else if (text.includes("화산")) {
+    return "화산";
+  } else if (text.includes("낙뢰")) {
+    return "낙뢰";
+  } else if (text.includes("건조")) {
+    return "건조";
+  } else if (text.includes("병충해")) {
+    return "병충해";
+  } else if (text.includes("안개")) {
+    return "안개";
+  } else if (text.includes("폭염")) {
+    return "폭염";
+  }
+
+  if (text.includes("화재")) {
+    return "화재";
+  } else if (text.includes("교통사고")) {
+    return "교통사고";
+  } else if (text.includes("단수")) {
+    return "단수";
+  }else if (text.includes("산불")) {
+    return "산불";
+  } else if (text.includes("익사")) {
+    return "익사";
+  } else if (text.includes("폭발")) {
+    return "폭발";
+  } else if (text.includes("가스")) {
+    return "가스";
+  } else if (text.includes("붕괴")) {
+    return "붕괴";
+  } else if (text.includes("전기가스")) {
+    return "전기가스";
+  } else if (text.includes("상하수도")) {
+    return "상하수도";
+  } else if (text.includes("매몰")) {
+    return "매몰";
+  } else if (text.includes("방사능")) {
+    return "방사능재난";
+  } else if (
+    text.includes("화학물질") ||
+    text.includes("누출") ||
+    text.includes("화학사고")
+  ) {
+    return "유해화학물질 유출사고";
+  } else if (text.includes("정전")) {
+    return "정전";
+  } else if (text.includes("교통") || text.includes("도로")) {
+    return "교통";
+  } else if (text.includes("감염병") || text.includes("코로나")) {
+    return "감염병";
+  } else if (text.includes("식중독")) {
+    return "식중독";
+  } else {
+    return "기타";
+  }
+}
--- my-app/components/Message.tsx
+++ my-app/components/Message.tsx
@@ -1,15 +1,22 @@
 "use client";
 import MsgInfo from "@/models/MsgInfo";
+import getKeywords from "@/app/getKeywords";
+
 interface MessageProps {
   message: MsgInfo;
 }
 
 const Message = ({ message }: MessageProps) => {
+  const hashtag = getKeywords(message.msg);
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <div>{message.create_date}</div>
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+        {hashtag}
+      </span>
       <div>{message.location_name}</div>
       <p className="text-gray-700 text-base">{message.msg}</p>
+
+      <div>{message.create_date}</div>
     </div>
   );
 };
