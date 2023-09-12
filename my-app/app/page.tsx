dc45bb1376f27978caa48c5d77cf0995f0bfb635
Yoon Robin <robinyoonofficial@gmail.com>
2023-09-12 10:03:44 +0900

    Refactor: Add semantic tags

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -8,34 +8,32 @@ export default async function Home() {
   const data = await fetchMessages(1);
 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://github.com/robinyoon-dev/disaster-msg-project"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/github.svg"
-              alt="GitHub Logo"
-              className={styles.githubLogo}
-              width={24}
-              height={24}
-              priority
-            />{" "}
-            GitHub
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <MessagesList initialMessages={data} />
-      </div>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1>재난 메시지</h1>
+        <a
+          href="https://github.com/robinyoon-dev/disaster-msg-project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/github.svg"
+            alt="GitHub Logo"
+            width={24}
+            height={24}
+            priority
+          />{" "}
+          GitHub
+        </a>
+      </header>
+      <main className={styles.main}>
+        <section>
+          <MessagesList initialMessages={data} />
+        </section>
+        <section>
+          상세 정보
+        </section>
+      </main>
+    </>
   );
 }
--- my-app/components/Message.tsx
+++ my-app/components/Message.tsx
@@ -1,7 +1,6 @@
 "use client";
 import MsgInfo from "@/models/MsgInfo";
 interface MessageProps {
-  //   children: React.ReactNode;
   message: MsgInfo;
 }
 
@@ -10,7 +9,6 @@ const Message = ({ message }: MessageProps) => {
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <div>{message.create_date}</div>
       <div>{message.location_name}</div>
-      <div>{message.md101_sn}</div>
       <p className="text-gray-700 text-base">{message.msg}</p>
     </div>
   );
