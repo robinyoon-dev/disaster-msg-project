769ba178a2689331057a64a60a95e87ecdbf25ea
Yoon Robin <robinyoonofficial@gmail.com>
2023-09-13 09:05:40 +0900

    Style: Add padding and margin in card

--- my-app/components/Message.tsx
+++ my-app/components/Message.tsx
@@ -12,10 +12,10 @@ const Message = ({ message }: MessageProps) => {
   const hashtag = getKeywords(message.msg);
   const emoji = getEmoji(hashtag);
   return (
-    <div className="font-sans flex items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-      <div className=" rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg">
+    <div className="p-2 mb-2 font-sans flex items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+      <div className=" flex flex-col justify-center items-center md:h-auto md:w-48">
         <Image src={emoji} alt={hashtag} width={80} height={80} priority />
-        {hashtag}
+        <p className="font-bold">{hashtag}</p>
       </div>
       <div className="flex flex-col justify-between p-4 leading-normal">
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
