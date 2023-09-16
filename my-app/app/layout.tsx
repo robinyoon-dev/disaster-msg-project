7dab8e4117cfa5d0e7fb3406193408182dc5f2d2
Yoon Robin <robinyoonofficial@gmail.com>
2023-09-17 07:40:26 +0900

    Fix: Add data about open graph in medadata object

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,10 +1,38 @@
-import Head from "next/head";
 import "./globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "전국 재난 톡",
   description: "전국 각 지역에서 발송된 재난 메시지를 볼 수 있습니다.",
+  openGraph: {
+    title: "전국 재난 톡",
+    description: "전국 각 지역에서 발송된 재난 메시지를 볼 수 있습니다.",
+    type: "website",
+    url: "https://disaster-msg-project.vercel.app/",
+    locale: "ko_KR",
+    images: [
+      {
+        url: "https://disaster-msg-project.vercel.app/ogimage.png",
+        width: 1200,
+        height: 630,
+        alt: "전국 재난 톡",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "전국 재난 톡",
+    description: "전국 각 지역에서 발송된 재난 메시지를 볼 수 있습니다.",
+    creator: "@robinyoondev",
+    images: [
+      {
+        url: "https://disaster-msg-project.vercel.app/ogimage.png",
+        width: 1200,
+        height: 630,
+        alt: "전국 재난 톡",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -14,48 +42,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko" className="dark">
-      <Head>
-        {/* Open Graph */}
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:url"
-          content="https://disaster-msg-project.vercel.app/"
-        />
-        <meta property="og:title" content="전국 재난 톡" />
-
-        <meta
-          property="og:description"
-          content="전국 각 지역에서 발송된 재난 메시지를 볼 수 있습니다."
-        />
-        <meta property="og:site_name" content="전국 재난 톡" />
-        <meta property="og:locale" content="ko_KR" />
-
-        <meta
-          property="og:image"
-          content="https://disaster-msg-project.vercel.app/ogimage.png"
-        />
-        <meta property="og:image:type" content=".png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" content="About 전국 재난 톡" />
-
-        {/* twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="전국 재난 톡" />
-        <meta name="twitter:text:title" content="전국 재난 톡" />
-        <meta
-          name="twitter:description"
-          content="전국 각 지역에서 발송된 재난 메시지를 볼 수 있습니다."
-        />
-        <meta
-          name="twitter:image"
-          content="https://disaster-msg-project.vercel.app/ogimage.png"
-        />
-        <meta name="twitter:image:type" content=".png" />
-        <meta name="twitter:image:width" content="1200" />
-        <meta name="twitter:image:height" content="630" />
-        <meta property="twitter:image:alt" content="About 전국 재난 톡" />
-      </Head>
       <body className="dark:bg-gray-900">{children}</body>
     </html>
   );
