41cdc95f0c8d3c764c0e9dff934030f786d2ac08
Yoon Robin <robinyoonofficial@gmail.com>
2023-09-17 07:27:31 +0900

    Fix: Fix metadata

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -22,18 +22,22 @@ export default function RootLayout({
           content="https://disaster-msg-project.vercel.app/"
         />
         <meta property="og:title" content="전국 재난 톡" />
-        <meta
-          property="og:image"
-          content="https://disaster-msg-project.vercel.app/ogimage.png"
-        />
+
         <meta
           property="og:description"
           content="전국 각 지역에서 발송된 재난 메시지를 볼 수 있습니다."
         />
         <meta property="og:site_name" content="전국 재난 톡" />
         <meta property="og:locale" content="ko_KR" />
+
+        <meta
+          property="og:image"
+          content="https://disaster-msg-project.vercel.app/ogimage.png"
+        />
+        <meta property="og:image:type" content=".png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="About 전국 재난 톡" />
 
         {/* twitter */}
         <meta name="twitter:card" content="summary_large_image" />
@@ -47,6 +51,10 @@ export default function RootLayout({
           name="twitter:image"
           content="https://disaster-msg-project.vercel.app/ogimage.png"
         />
+        <meta name="twitter:image:type" content=".png" />
+        <meta name="twitter:image:width" content="1200" />
+        <meta name="twitter:image:height" content="630" />
+        <meta property="twitter:image:alt" content="About 전국 재난 톡" />
       </Head>
       <body className="dark:bg-gray-900">{children}</body>
     </html>
