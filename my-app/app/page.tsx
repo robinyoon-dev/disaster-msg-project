23c8817aba0a8c297769949ab7e261b7c6d190ab
Yoon Robin <robinyoonofficial@gmail.com>
2023-10-06 18:04:44 +0900

    Feat: Add scroll back to top button (only style)

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,7 +2,6 @@ import MessagesList from "@/components/MessagesList";
 import Nav from "@/components/Nav";
 import getData from "./actions";
 
-
 export default async function Home() {
   process.env.__NEXT_PRIVATE_PREBUNDLED_REACT = "experimental";
   const data = await getData(1);
@@ -20,6 +19,19 @@ export default async function Home() {
         <section>
           <MessagesList initialMessages={initialMessages} />
         </section>
+
+        {/* hidden  */}
+        <button className="fixed z-50 bottom-10 right-10 p-4 border-0 w-14 h-14 rounded-full shadow-md bg-gray-600 hover:bg-gray-700 text-white text-lg font-semibold transition-colors duration-300">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="w-6 h-6"
+          >
+            <path d="M12 4l8 8h-6v8h-4v-8H4l8-8z" />
+          </svg>
+          <span className="sr-only">Go to top</span>
+        </button>
       </main>
     </div>
   );
