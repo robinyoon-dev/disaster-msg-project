673eee5777706f28e9596094c92677989a0d90fc
Yoon Robin <robinyoonofficial@gmail.com>
2023-11-13 09:19:02 +0900

    Fix: Remove redundancy of locations

--- my-app/app/getLocationArray.ts
+++ my-app/app/getLocationArray.ts
@@ -1,3 +1,11 @@
 export default function getLocationArray(locations: string) {
-  return locations.split(",");
+  return removeRedundancy(locations.split(","));
+}
+
+
+function removeRedundancy(arr: string[]){
+  const set = new Set(arr);
+  console.log(set.keys());
+  console.log(Array.from(set.keys()));
+  return Array.from(set.keys());
 }
